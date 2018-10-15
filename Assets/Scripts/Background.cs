f1bfd42155c78dc70fad0c25ceeaf92bef180193
jkwatson92 <jkwatson@umich.edu>
2018-10-15 13:29:59 -0400

    Adding sounds
    Added some spoopy effects and background music

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    public AudioClip background;
+    public AudioSource backgroundSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        backgroundSource.clip = background;
+        backgroundSource.volume = 0.7f;
+        backgroundSource.Play();
+    }
+
+}
