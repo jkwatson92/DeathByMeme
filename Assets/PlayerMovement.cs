bb77f8e0e4382133c1fabe7623c3e0d6c11a16ef
LindsayPayton <lhilliar@umich.edu>
2018-10-16 18:19:03 -0400

    Script
    MC Animation & Movement
    Camera sizing & Movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,21 +9,21 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		rbody = GetComponent<Rigidbody2D> ();
-		anim = GetComponent<Animator> ();
+        rbody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 movement_V = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+		Vector2 movement_V = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		if (movement_V != Vector2.zero) {
 			anim.SetBool("Is_walking", true);
-			anim.SetFloat("Input_x", movement_V.x);
-			anim.SetFloat("Input_y", movement_V.y);
+            anim.SetFloat("Input_x", movement_V.x);
+            anim.SetFloat("Input_y", movement_V.y);
 		} else {
 			anim.SetBool("Is_walking", false);
 		}
 		
-		rbody.MovePosition (rbody.position + movement_V * Time.deltaTime);
+        rbody.MovePosition(rbody.position + movement_V * Time.deltaTime * 100.0f);
 	}
 }
---
+++ Assets/cameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+    Camera myCam;
+
+	// Use this for initialization
+	void Start () {
+        myCam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myCam.orthographicSize = (Screen.height / 100f) / .05f;
+        if(target){
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3(0,0,-10);
+        }
+	}
+}
