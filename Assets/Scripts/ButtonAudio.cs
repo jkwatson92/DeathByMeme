c6f5bb3945e607ecd8afc93ff76548e88c64c0d2
jkwatson92 <jkwatson@umich.edu>
2018-10-24 22:37:35 -0400

    Main Menu
    Added Main Menu items, need to link Start and Quit buttons to respective actions

---
+++ Assets/Scripts/ButtonAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudio : MonoBehaviour
+{
+    public AudioClip effect;
+    public AudioSource effectSource;
+
+
+    public void onHover()
+    {
+        effectSource.clip = effect;
+        effectSource.Play();
+    }
+}
---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ButtonHandler : MonoBehaviour
+{
+    public Text buttonText;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public void onHover()
+    {
+        buttonText.color = Color.blue;
+    }
+
+    public void OnMouseExit()
+    {
+        buttonText.color = Color.red;
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("First Floor", LoadSceneMode.Single);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
