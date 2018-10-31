b1666a2bc44fc74c147127af4faa338d8c490696
jkwatson92 <jkwatson@umich.edu>
2018-10-31 15:46:39 -0400

    buttons do something now

---
+++ Assets/Scripts/GameOverButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButtons : MonoBehaviour {
+    public bool mainMenu;
+    public bool reset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnMouseUp()
+    {
+        if (mainMenu)
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+        if (reset)
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+    }
+}
