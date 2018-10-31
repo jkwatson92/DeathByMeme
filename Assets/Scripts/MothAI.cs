55c89a6a0e97a1e234efbc6a0a8732786bc1ef1e
LindsayPayton <lhilliar@umich.edu>
2018-10-31 13:09:18 -0400

    Moth Movement

--- Assets/Scripts/MothAI.cs
+++ Assets/Scripts/MothAI.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MothAI : MonoBehaviour {
-
     public Transform[] EnemyPoints;
     Transform currentPoint;
     int currentPointIndex;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         currentPointIndex = 0;
         currentPoint = EnemyPoints[currentPointIndex];
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector3.up * Time.deltaTime * 50f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * 10f);
         if(Vector3.Distance(transform.position, currentPoint.position)<.1f){
             if (currentPointIndex + 1 < EnemyPoints.Length)
             {
@@ -29,6 +30,9 @@ public class MothAI : MonoBehaviour {
         }
         Vector3 direction = currentPoint.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg -90f;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
 
-	}
+    }
+
 }
---
+++ Assets/Scripts/MothAI2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MothAI2 : MonoBehaviour {
+
+    float savedTime = Time.time;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float counter = Time.time - savedTime;
+        if (counter <= 5)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 50f);
+        }
+        else if (counter > 5 && counter <= 10)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * 50f);
+        }
+        else if (counter > 10 && counter <= 15)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 35f);
+        }
+        else if (counter > 15 && counter <= 25)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 35f);
+        }
+        else if (counter > 25 && counter <= 30)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 35f);
+        }
+        else
+        {
+            savedTime = Time.time;
+        }
+    }
+}
